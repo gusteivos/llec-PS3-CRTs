14ebdd729877a90c44a0e15228cee61bee9057ac
Gustavo Ramos Carvalho <gc5142387@gmail.com>
2025-12-23 01:38:04 -0300

    ppu: PSL1GHT-ppu.sh uses __PSL1GHT__OPD

--- PSL1GHT-ppu.sh
+++ PSL1GHT-ppu.sh
@@ -6,11 +6,10 @@ export PSL1GHT=$PS3DEV
 ## Set up the path.
 export PATH=$PATH:$PS3DEV/bin
 export PATH=$PATH:$PS3DEV/ppu/bin
-export PATH=$PATH:$PS3DEV/spu/bin
 
 
 ppu-gcc -c ppu/llec_ppu_crt0.c -o llec_ppu_crt0.c.o -Ippu/include/
-ppu-gcc -c ppu/llec_ppu_crt0.S -o llec_ppu_crt0.S.o -Ippu/include/ -D__ASSEMBLY__ -D__PSL1GHT
+ppu-gcc -c ppu/llec_ppu_crt0.S -o llec_ppu_crt0.S.o -Ippu/include/ -D__ASSEMBLY__ -D__PSL1GHT -D__PSL1GHT__OPD
 
 
 ppu-ld -r llec_ppu_crt0.c.o llec_ppu_crt0.S.o PSL1GHT-ppu-crt1.o -o llec-PSL1GHT-ppu-crt0.o
